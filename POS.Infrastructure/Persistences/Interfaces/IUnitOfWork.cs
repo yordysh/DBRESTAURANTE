8236aa4f77b5e24b8b1c02f9ce3bc6923ee0c384
yordy <yordysh28@gmail.com>
2023-02-23 17:32:13 -0500

    creating the repository of genres and also pagination

---
+++ POS.Infrastructure/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Infrastructure.Persistences.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        // Declaracion o matricula de nuestra interfaces a nivel de repository
+        ICategoryRepository Category { get; }
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
---
+++ POS.Infrastructure/Persistences/Repositories/CategoryRepository.cs
@@ -0,0 +1,60 @@
+﻿using POS.Domain.Entities;
+using POS.Infrastructure.Persistences.Contexts;
+using POS.Infrastructure.Persistences.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Infrastructure.Persistences.Repositories
+{
+    public class CategoryRepository : GenericRepository<Categoria>, ICategoryRepository
+    {
+        //public CategoryRepository(DBRESTAURANTEContext context) : base(context)
+        //{
+
+        //}
+
+        //public async Task<BaseEntityResponse<Category>> ListCategories(BaseFiltersRequest filters)
+        //{
+        //    var response = new BaseEntityResponse<Category>();
+
+
+        //    var categories = GetEntityQuery(x => x.AuditDeleteUser == null && x.AuditDeleteDate == null);
+
+        //    if (filters.NumFilter is not null && !string.IsNullOrEmpty(filters.TextFilter))
+        //    {
+        //        switch (filters.NumFilter)
+        //        {
+        //            case 1:
+        //                categories = categories.Where(x => x.Name!.Contains(filters.TextFilter));
+        //                break;
+        //            case 2:
+        //                categories = categories.Where(x => x.Description!.Contains(filters.TextFilter));
+        //                break;
+
+        //        }
+        //    }
+        //    if (filters.StateFilter is not null)
+        //    {
+        //        categories = categories.Where(x => x.State.Equals(filters.StateFilter));
+        //    }
+        //    if (!string.IsNullOrEmpty(filters.StartDate) && !string.IsNullOrEmpty(filters.EndDate))
+        //    {
+        //        categories = categories.Where(x => x.AuditCreateDate >= Convert.ToDateTime(filters.StartDate) && x.AuditCreateDate <= Convert.ToDateTime(filters.EndDate).AddDays(1));
+        //    }
+        //    if (filters.Sort is null) filters.Sort = "CategoryId";
+        //    response.TotalRecords = await categories.CountAsync();
+        //    response.Items = await Ordering(filters, categories, !(bool)filters.Download!).ToListAsync();
+        //    return response;
+        //}
+
+
+        private readonly DBRESTAURANTEContext _context;
+        public CategoryRepository(DBRESTAURANTEContext context)
+        {
+            _context = context;
+        }
+    }
+}
